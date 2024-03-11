c2686dcf91153555fb19c54bb61af00645c869fe
Harrison Chen <hchen.robotics@gmail.com>
2024-03-11 12:34:27 -0400

    Nvidia Drivers + Dotfiles Install + Firefox Fix
    - Add command to automatically detect and install Nvidia drivers
    - Dotfiles are now cloned and installed as a part of the
      packages_install.sh script, with remotes for dotfiles and linux-setup
      repos changed to SSH
    - Add package to Firefox install command
    - TPM installs plugins automatically

--- packages_install.sh
+++ packages_install.sh
@@ -13,6 +13,11 @@ sudo apt update && sudo apt install -y \
     tmux \
     ubuntu-restricted-extras
 
+# Change origin to SSH
+script_dir=$(dirname $0)
+cd $script_dir
+git remote set-url origin git@github.com:chen-harrison/linux-setup.git
+
 # TLDR
 sudo pip3 install tldr numpy
 
@@ -24,5 +29,15 @@ else
     echo "No input received, skipping ssh-keygen"
 fi
 
+# Dotfiles
+git clone https://github.com/chen-harrison/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+git remote set-url origin git@github.com:chen-harrison/dotfiles.git
+./install.sh
+
 # TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/install_plugins
+
+# Nvidia drivers
+sudo ubuntu-drivers install
