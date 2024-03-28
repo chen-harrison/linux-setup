0c5045cc246ccd8480d2c691318edd21d4e6a34d
Harrison Chen <hchen.robotics@gmail.com>
2024-03-28 14:20:46 -0400

    Create settings_install.sh
    Reorganizing scripts more logically

--- full_install.sh
+++ full_install.sh
@@ -3,22 +3,9 @@
 script_dir=$(dirname $0)
 cd $script_dir
 
-# Ubuntu 22.04 video playback fix
-read -r -p "Is this Ubuntu 22.04 [y/N]? "
-if [[ "$REPLY" =~ ^[yY]$ ]] ; then
-    echo "Fixing video playback"
-    sudo apt remove -y gstreamer1.0-vaapi
-fi
-
-# Dual boot clock correction
-read -r -p "Is this a dual-boot configuration [y/N]? "
-if [[ "$REPLY" =~ ^[yY]$ ]] ; then
-    echo "Fixing clock"
-    timedatectl set-local-rtc 1 --adjust-system-clock
-fi
-
 ./packages_install.sh
 ./apps_install.sh
+./settings_install.sh
 # ./dev_tools_install.sh
 
 echo \ "
