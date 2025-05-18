cd6cae00c2ee4d00a2a6062cbca3c8d830c19a0e
Harrison Chen <hchen.robotics@gmail.com>
2025-05-17 23:13:35 -0400

    Add lazygit

--- dev_tools_install.sh
+++ dev_tools_install.sh
@@ -49,6 +49,13 @@ sudo apt-get install -y \
 
 (sudo groupadd docker ; sudo usermod -aG docker $USER ) || true
 
+# Lazygit
+mkdir -p ~/.local/bin
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz -C ~/.local/bin lazygit
+rm lazygit.tar.gz
+
 # LazyDocker
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
