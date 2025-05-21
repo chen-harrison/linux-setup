c0115f8680a686ad7c9edce2a5f298d2d56000b6
Harrison Chen <hchen.robotics@gmail.com>
2025-05-21 11:46:15 -0400

    Add htop + nvtop packages

--- packages_install.sh
+++ packages_install.sh
@@ -11,8 +11,10 @@ sudo apt-get update && sudo apt-get install -y \
     git \
     gdb \
     gthumb \
+    htop \
     imagemagick \
     latexmk \
+    nvtop \
     ripgrep \
     shellcheck \
     synaptic \
