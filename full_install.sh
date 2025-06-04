56d3defa7e0989dd71097546d53169b86905a666
Harrison Chen <hchen.robotics@gmail.com>
2025-06-03 23:57:08 -0400

    Reorganize installation scripts
    - Create new personal_install.sh that includes Discord and other
      installations that would only go on a personal machine
    - full_install.sh asks if the install is for personal or professional
      use, which determines whether or not to trigger personal_install.sh
    - Run dev_tools_install.sh by default
    - Comment out WideVineCdm plugin fix (for now, to see if needed) and
      Google Chrome installation

--- full_install.sh
+++ full_install.sh
@@ -2,16 +2,27 @@
 
 cd "$(dirname $0)"
 
+# Change remote origin to SSH + configure user
+cd $(dirname $0)
+git remote set-url origin git@github.com:chen-harrison/linux-setup.git
+git config --local user.name "Harrison Chen"
+git config --local user.email "hchen.robotics@gmail.com"
+
+read -r -p "Is this installation for personal (not professional) use [y/N]? " personal
+
 ./packages_install.sh
 ./apps_install.sh
 ./settings_install.sh
-# ./dev_tools_install.sh
+./dev_tools_install.sh
 ./drivers_install.sh
 ./dotfiles_install.sh
+
+if [[ "$personal" =~ ^[yY]([eE][sS])?$ ]] ; then
+    ./personal_install.sh
+fi
 
 echo \ "
 To-Do:
 - Restart to apply Nvidia drivers
 - Add public SSH keys where needed (e.g. GitHub)
-- ./dev_tools_install.sh
 "
