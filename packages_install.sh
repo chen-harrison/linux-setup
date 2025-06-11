49edc61142a9afc7a7309c1e43a6d504e7a382d8
Harrison Chen <hchen.robotics@gmail.com>
2025-06-10 21:58:22 -0400

    Create terminal_tools_install.sh
    - Separate packages_install.sh into two scripts, making the terminal
      tools like fasd, fzf, and nnn to be installed and used easily on other
      devices or environments that can't utilize the whole installation
      pipeline

--- packages_install.sh
+++ packages_install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Make sure this directory exists
-mkdir -p ~/.local/bin
-
 # Packages
 sudo apt-get update && sudo apt-get install -y \
     python3-pip \
@@ -26,27 +23,6 @@ sudo apt-get update && sudo apt-get install -y \
     ubuntu-restricted-extras \
     xsel
 
-# fd
-sudo apt-get install -y fd-find
-ln -s "$(which fdfind)" ~/.local/bin/fd
-
-# fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --key-bindings --completion --no-update-rc
-
-# fasd
-wget -O fasd.zip https://github.com/clvv/fasd/archive/refs/tags/1.0.1.zip
-unzip fasd.zip
-cd fasd-1.0.1 && sudo make install && cd ..
-rm -r fasd.zip fasd-1.0.1
-
-# tldr-pages
-sudo apt-get install pipx
-pipx ensurepath
-pipx install tldr
-
-# TODO: fix thefuck installation
-
 # yt-dlp
 wget -O ~/.local/bin/yt-dlp https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
 chmod +x ~/.local/bin/yt-dlp
@@ -55,23 +31,6 @@ chmod +x ~/.local/bin/yt-dlp
 wget https://github.com/WGUNDERWOOD/tex-fmt/releases/latest/download/tex-fmt-x86_64-linux.tar.gz
 sudo tar -xvzf tex-fmt-x86_64-linux.tar.gz --directory=/usr/bin
 rm tex-fmt-x86_64-linux.tar.gz
-
-# Nerd Fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/UbuntuMono.zip
-unzip UbuntuMono.zip -d UbuntuMono
-sudo mv UbuntuMono /usr/share/fonts/truetype
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/DroidSansMono.zip
-unzip DroidSansMono.zip -d DroidSansMono
-sudo mv DroidSansMono /usr/share/fonts/opentype
-fc-cache -f
-rm UbuntuMono.zip DroidSansMono.zip
-
-# nnn
-git clone https://github.com/jarun/nnn.git
-cd nnn && git tag --sort=-creatordate | head -n1 | xargs git checkout
-sudo apt-get install -y pkg-config libncursesw5-dev libreadline-dev
-sudo make strip install O_NERD=1
-cd .. && rm -rf nnn
 
 # SSH key
 read -r -p "Email address for SSH key: "
