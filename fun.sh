075df3ab16422ac5508aa8ecd4be8ad08d96602f
Harrison Chen <hchen.robotics@gmail.com>
2026-07-05 22:09:29 -0400

    Update melonDS installation
    - Remove melonDS.zip
    - Update package requirements for Ubuntu 24.04
    - Grab binary + icon from GitHub, write .desktop file in script

--- fun.sh
+++ fun.sh
@@ -8,17 +8,33 @@ sudo apt-get update && sudo apt-get install -y \
     libcurl4-gnutls-dev \
     libpcap0.8-dev \
     libsdl2-dev \
-    qtbase5-dev \
-    qtbase5-private-dev \
-    qtmultimedia5-dev \
     libarchive-dev \
+    libenet-dev \
     libzstd-dev \
-    libslirp0
+    libfaad-dev
 
-unzip melonDS.zip
-sed -i "s/\$USER/$USER/g" melonDS/melonDS.desktop
-mv melonDS ~/.local/share/
-ln -s ~/.local/share/melonDS/melonDS.desktop ~/.local/share/applications/melonDS.desktop
+# Ubuntu 24.04 requirements
+sudo apt-get install -y qt6-{base,base-private,multimedia,svg}-dev
+
+melonds_dir=~/.local/share/melonDS
+melonds_url=$(curl -fsSL https://api.github.com/repos/melonDS-emu/melonDS/releases/latest | jq -r '.assets[].browser_download_url' | grep -e "melonDS-.*ubuntu-x86_64.zip")
+wget -O melonds.zip "$melonds_url"
+mkdir -p "$melonds_dir"
+unzip melonds.zip -d "$melonds_dir"
+wget -O "${melonds_dir}/melonDS.png" https://raw.githubusercontent.com/melonDS-emu/melonDS/master/res/icon/melon_256x256.png
+
+cat > "${melonds_dir}/melonDS.desktop" << EOF
+[Desktop Entry]
+Name=melonDS
+Exec=/home/$USER/.local/share/melonDS/melonDS
+StartupNotify=true
+Terminal=false
+Type=Application
+Categories=Game;
+Icon=/home/$USER/.local/share/melonDS/melonDS.png
+EOF
+
+ln -sf "${melonds_dir}/melonDS.desktop" ~/.local/share/applications/melonDS.desktop
 
 # Discord
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
